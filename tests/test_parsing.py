83c876904b096ea228581b6190b45f36a886fccb
Michael Beaton <beatonma@gmail.com>
2020-01-26 18:58:50 +0000

    Rewritten to reflect changes in main script. Particularly, most functions now accept and/or return a Dict[str, PyClass] instead of a List[Model].

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -6,13 +6,15 @@ import logging
 from unittest import TestCase
 
 from model_class_dependencies import (
-    build_model_references,
+    inherit_mixin_fields,
     parse_field,
     parse_field_params,
-    parse_models,
+    parse_classes,
 )
 from .data.data_parsing import *
 
+mpl_logger = logging.getLogger('matplotlib')
+mpl_logger.setLevel(logging.WARNING)
 log = logging.getLogger(__name__)
 
 
@@ -60,49 +62,59 @@ class ParsingTests(TestCase):
         self.assertEqual(multiline.kwargs["on_delete"], "models.DELETE")
         self.assertEqual(multiline.kwargs["related_name"], "constituencies")
 
-    def test_parse_models(self):
-        simple = parse_models(SIMPLE_MODEL)[0]
+    def test_parse_classes(self):
+        simple = parse_classes(SIMPLE_MODEL)["Simple"]
         self.assertEqual("Simple", simple.name)
         self.assertEqual("models.Model", simple.class_dependencies[0])
 
-        complex = parse_models(COMPLEX_MODEL)[0]
-        self.assertEqual(complex.name, "Bill")
-        self.assertListEqual(
-            complex.class_dependencies, ["ParliamentDotUkMixin", "BaseModel"]
-        )
-        self.assertEqual(len(complex.fields), 9)
+        _complex = parse_classes(COMPLEX_MODEL)["Bill"]
+        self.assertListEqual([], _complex.foreign_key_fields())
 
-        # bill_chapter = models.PositiveIntegerField(default=0)
-        bill_chapter = complex.fields[5]
+        _complex.is_model = True
+        self.assertListEqual(
+            ["ParliamentDotUkMixin", "BaseModel"],
+            _complex.class_dependencies
+        )
+        self.assertEqual(len(_complex.fields), 9)
+
+        bill_chapter = _complex.fields[5]
         self.assertEqual("bill_chapter", bill_chapter.name)
         self.assertEqual("models.PositiveIntegerField", bill_chapter.type)
         self.assertEqual([], bill_chapter.args)
         self.assertEqual("0", bill_chapter.kwargs["default"])
 
-        self.assertListEqual(["BillType", "ParliamentarySession"], complex.fk_models())
+        self.assertListEqual(
+            ["BillType", "ParliamentarySession"],
+            _complex.fk_models()
+        )
 
-        models = parse_models(MULTIPLE_MODELS)
-        self.assertEqual(3, len(models))
+        models = parse_classes(MULTIPLE_MODELS)
+        self.assertEqual(3, len(models.values()))
 
-        sitting, publication, bill = models
-        self.assertEqual("BillStageSitting", sitting.name)
-        self.assertEqual("BillPublication", publication.name)
-        self.assertEqual("Bill", bill.name)
+        sitting = models["BillStageSitting"]
+        publication = models["BillPublication"]
+        bill = models["Bill"]
 
         self.assertEqual(4, len(sitting.fields))
         self.assertEqual(2, len(publication.fields))
         self.assertEqual(9, len(bill.fields))
 
-    def test_mixin_inclusion(self):
+    def test_inherit_mixin_fields(self):
         """If a model has mixins, and those mixins are discovered, then any fields
         from those mixins should be added to modelfields. """
-        models = parse_models(MODEL_WITH_MIXIN)
-        self.assertEqual(2, len(models))
+        models = parse_classes(MODEL_WITH_MIXIN)
+        for x in models.values():
+            x.is_model = True
 
-        puk, publication = models
+        self.assertEqual(3, len(models.values()))
+
+        publication = models["BillPublication"]
         self.assertEqual(2, len(publication.fields))
 
-        build_model_references(models)
-        puk, publication = models
+        inherit_mixin_fields(models)
+
         self.assertEqual(3, len(publication.fields))
         self.assertTrue('parliamentdotuk' in [x.name for x in publication.fields])
+
+        print(publication.fk_models())
+        self.assertTrue('Bill' in publication.fk_models())
